6d26e4f6e3dfa9f6829f31900761e1e53be93903
daniilmoroz.work <daniilmoroz.work@gmail.com>
2025-03-16 16:58:27 +0200

    Basic Section functional

--- Trello_winforms/Form1.Designer.cs
+++ Trello_winforms/Form1.Designer.cs
@@ -28,21 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.contextMenuStrip2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(161, 33);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.ReadOnly = true;
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 27);
+            this.toolStripTextBox1.Text = "New Section";
+            this.toolStripTextBox1.Click += new System.EventHandler(this.toolStripTextBox1_Click);
+            this.toolStripTextBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.toolStripTextBox1_MouseDown);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(582, 353);
+            this.MaximumSize = new System.Drawing.Size(1300, 400);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Winllo";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.contextMenuStrip2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
     }
 }
 
---
+++ Trello_winforms/Sections.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class Sections
+{
+    private Control parent; 
+    private int sectionWidth = 150; 
+    private int sectionSpacing = 5; 
+    private ContextMenuStrip sectionMenu; 
+
+    public Sections(Control parentControl)
+    {
+        parent = parentControl;
+        InitializeSectionMenu();
+    }
+
+    private void InitializeSectionMenu()
+    {
+        sectionMenu = new ContextMenuStrip();
+
+        ToolStripMenuItem renameItem = new ToolStripMenuItem("Rename");
+        renameItem.Click += RenameSection;
+
+        ToolStripMenuItem changeColorItem = new ToolStripMenuItem("Change color");
+        changeColorItem.Click += ChangeSectionColor;
+
+        ToolStripMenuItem deleteItem = new ToolStripMenuItem("Delete section");
+        deleteItem.Click += DeleteSection;
+
+        sectionMenu.Items.Add(renameItem);
+        sectionMenu.Items.Add(changeColorItem);
+        sectionMenu.Items.Add(deleteItem);
+    }
+
+    public void AddSection()
+    {
+        Panel section = new Panel
+        {
+            Width = sectionWidth,
+            Height = parent.ClientSize.Height,
+            BackColor = Color.LightGray,
+            BorderStyle = BorderStyle.FixedSingle,
+            ContextMenuStrip = sectionMenu 
+        };
+
+        Label titleLabel = new Label
+        {
+            Text = "New Section",
+            Dock = DockStyle.Top,
+            TextAlign = ContentAlignment.MiddleCenter,
+            AutoSize = false,
+            Height = 30,
+            BackColor = Color.DarkGray,
+            ForeColor = Color.White,
+            ContextMenuStrip = sectionMenu 
+        };
+
+        section.Controls.Add(titleLabel);
+
+        int newX = 10;
+
+        foreach (Control ctrl in parent.Controls)
+        {
+            if (ctrl is Panel)
+            {
+                newX = ctrl.Right + sectionSpacing;
+            }
+        }
+
+        section.Location = new Point(newX, 0);
+        section.Tag = titleLabel; 
+        parent.Controls.Add(section);
+    }
+
+    private void RenameSection(object sender, EventArgs e)
+    {
+        if (sectionMenu.SourceControl is Panel section)
+        {
+            Label titleLabel = (Label)section.Tag;
+            string newName = Microsoft.VisualBasic.Interaction.InputBox("Enter new section name:", "Renaming", titleLabel.Text);
+            if (!string.IsNullOrWhiteSpace(newName))
+            {
+                titleLabel.Text = newName;
+            }
+        }
+    }
+
+    private void ChangeSectionColor(object sender, EventArgs e)
+    {
+        if (sectionMenu.SourceControl is Panel section)
+        {
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    section.BackColor = colorDialog.Color;
+                }
+            }
+        }
+    }
+
+    private void DeleteSection(object sender, EventArgs e)
+    {
+        if (sectionMenu.SourceControl is Panel section)
+        {
+            parent.Controls.Remove(section);
+            section.Dispose();
+
+            RepositionSections();
+        }
+    }
+
+    private void RepositionSections()
+    {
+        int newX = 10;
+
+        foreach (Control ctrl in parent.Controls)
+        {
+            if (ctrl is Panel section)
+            {
+                section.Location = new Point(newX, 0);
+                newX = section.Right + sectionSpacing;
+            }
+        }
+    }
+
+}
